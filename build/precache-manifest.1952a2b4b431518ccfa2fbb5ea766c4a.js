d99ad7e874bb752859e1da56c2272b0963199f21
Margaret Staszewska <megfa@yahoo.com>
2019-01-07 14:47:49 +0100

    Add files via upload

---
+++ build/precache-manifest.1952a2b4b431518ccfa2fbb5ea766c4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db9e1e2f456dbd9faa0c",
+    "url": "/static/js/main.db9e1e2f.chunk.js"
+  },
+  {
+    "revision": "456fbd52ef6b4a9db8c6",
+    "url": "/static/js/1.456fbd52.chunk.js"
+  },
+  {
+    "revision": "db9e1e2f456dbd9faa0c",
+    "url": "/static/css/main.bad01777.chunk.css"
+  },
+  {
+    "revision": "f5d2613e3c9a5d914f4975110f309cdb",
+    "url": "/index.html"
+  }
+];

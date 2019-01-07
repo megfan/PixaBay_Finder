88d30996cd23c3bb6a4396e17a0bb0e417db2262
megfan <megfa@yahoo.com>
2019-01-07 15:39:37 +0100

    Updates

---
+++ precache-manifest.585bedecb2972a2e7c4e4c13aeb3445a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27b810b017cbe3d27c7b",
+    "url": "/PixaBay_Finder/static/js/runtime~main.27b810b0.js"
+  },
+  {
+    "revision": "98bcc09135ea8484cc91",
+    "url": "/PixaBay_Finder/static/js/main.98bcc091.chunk.js"
+  },
+  {
+    "revision": "456fbd52ef6b4a9db8c6",
+    "url": "/PixaBay_Finder/static/js/1.456fbd52.chunk.js"
+  },
+  {
+    "revision": "98bcc09135ea8484cc91",
+    "url": "/PixaBay_Finder/static/css/main.bad01777.chunk.css"
+  },
+  {
+    "revision": "574d6e35bc0ae35fe36a740aecaa876e",
+    "url": "/PixaBay_Finder/index.html"
+  }
+];
